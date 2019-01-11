913e5bab13f12ddb58648395598bc04d58b031b0
Nicole W <nicoleewhitee@gmail.com>
2019-01-11 15:10:18 -0800

    variables added

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//Global Variables like scores
+
+var crystalBlueNum;
+var crystalGreenNum;
+var crystalYellowNum;
+var crystalRedNum;
+var winNum = 0;
+var loseNum = 0;
+var randomNum;
+
+$(document).ready(function() {
+    startGame();
+
+}
