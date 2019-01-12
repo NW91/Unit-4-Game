9ba7acde552da39bad0c377a350da4365be4fc5f
Nicole W <nicoleewhitee@gmail.com>
2019-01-12 03:41:20 -0800

    fixing crystal ids

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,25 +7,55 @@ var crystalYellowNum;
 var crystalRedNum;
 //Score Variables
 var gameStarted = false;
-var playerScore = 0;
 var winNum = 0;
 var loseNum = 0;
+var playerScore = 0;
 var randomNum;
 
 //Loads the Page
 $(document).ready(function() {
     startGame();
 
-//Start Game
+//Click listeners for all 4 crystals
+    $("#blue-crystal-icon-img").on("click", function() {
+        playerScore += crystalBlueNum;
+        $("#total-player-num").text(playerScore);
+        $("won-lost-notifier").text("");
+        setWinLose();
+    });
+
+    $("#green-crystal-icon-img").on("click", function() {
+        playerScore += crystalGreenNum;
+        $("#total-player-num").text(playerScore);
+        $("won-lost-notifier").text("");
+        setWinLose();
+    });
+
+    $("#yellow-crystal-icon-img").on("click", function() {
+        playerScore += crystalYellowNum;
+        $("#total-player-num").text(playerScore);
+        $("won-lost-notifier").text("");
+        setWinLose();
+    });
+
+    $("#red-crystal-icon-img").on("click", function() {
+        playerScore += crystalRedNum;
+        $("#total-player-num").text(playerScore);
+        $("won-lost-notifier").text("");
+        setWinLose();
+    });
+});
+
+//Start the Game
 function startGame() {
     gameStarted = true;
 
-//Generates random score at the start between 19 and 120
+//Generates random achievement score at the start between 19 and 120
 randomNum = Math.floor(Math.random() * 121)
 while (randomNum < 19) {
     randomNum = Math.floor(Math.random() * 121)
 }
-$("#randomGivenScore").text(randomNum);
+$("#random-given-score").text(randomNum);
 
 //Code to make the crystal values all different from each other and assign the crystals random values between 1 to 12
 while (
@@ -45,52 +75,21 @@ crystalRedNum = 1 + Math.floor(Math.random() * 12);
 
 //Player score starts at 0
 playerScore = 0;
-$("#player-score").text(playerScore);
+$("#total-player-num").text(playerScore);
 }
 
-//Click listeners for all 4 crystals
-    $("#blue-crystal-icon").on("click", function() {
-        playerScore += crystalBlueNum;
-        $("#player-score").text(playerScore);
-        $("won-Lost-Notifier").text("");
-        setWinLose();
-    });
-
-    $("#green-crystal-icon").on("click", function() {
-        playerScore += crystalGreenNum;
-        $("#player-score").text(playerScore);
-        $("won-Lost-Notifier").text("");
-        setWinLose();
-    });
-
-    $("#yellow-crystal-icon").on("click", function() {
-        playerScore += crystalYellowNum;
-        $("#player-score").text(playerScore);
-        $("won-Lost-Notifier").text("");
-        setWinLose();
-    });
-
-    $("#red-crystal-icon").on("click", function() {
-        playerScore += crystalRedNum;
-        $("#player-score").text(playerScore);
-        $("won-Lost-Notifier").text("");
-        setWinLose();
-    });
-});
-
 //Win-Loss Function Recorder and Restarts the Game
-function setWinLose(){
+function setWinLose() {
     if (playerScore === randomNum) {
         winNum++;
         $("#win-num").text(winNum);
-        $("#won-Lost-Notifier").text("Congratulations, you've won this round! Keep on playing!");
+        $("#won-lost-notifier").text("Congratulations, you've won this round! Keep on playing!");
         startGame();
     }
     else if (playerScore > randomNum) {
         loseNum++;
         $("#lose-num").text(loseNum);
-        $("#won-Lost-Notifier").text("Sorry but you've lost this round! Please try again!");
+        $("#won-lost-notifier").text("Sorry, but you've lost this round! Please try again!");
         startGame();
     }
-
 }
