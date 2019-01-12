bb6904f4c6ebe01930d33b7f5edcab9a0deaf902
Nicole W <nicoleewhitee@gmail.com>
2019-01-11 16:02:47 -0800

    row formats added to html and code added for random number generated in JavaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,56 @@
-//Global Variables like scores
+//JavaScript
+
+//Global Variables like scores and values
 
 var crystalBlueNum;
 var crystalGreenNum;
 var crystalYellowNum;
 var crystalRedNum;
+var playerScore = 0;
 var winNum = 0;
 var loseNum = 0;
-var randomNum;
+var randomNum = "";
 
 $(document).ready(function() {
     startGame();
+
+//Random Crystal values between 1 to 12
+var crystalBlueNum = Math.floor(Math.random() * 11+1);
+var crystalGreenNum = Math.floor(Math.random() * 11+1);
+var crystalYellowNum = Math.floor(Math.random() * 11+1);
+var crystalRedNum = Math.floor(Math.random() * 11+1);
+
+//Generates random score at the start between 19 and 120
+var randomNum = Math.floor(Math.random)() * 101+19)
+$("#randomNumber").text(Random);
+
+//Click listeners for all 4 crystals
+$("#Blue-Crystal").on("click", function(){
+    totalScoreNum += crystalBlueNum;
+    $("#total-score-num").text(totalScoreNum);
+    $("win-lose-message").text("");
+    setWinLose();
+});
+
+$("#Green-Crystal").on("click", function(){
+    totalScoreNum += crystalGreenNum;
+    $("#total-score-num").text(totalScoreNum);
+    $("win-lose-message").text("");
+    setWinLose();
+});
+
+$("#Yellow-Crystal").on("click", function(){
+    totalScoreNum += crystalYellowNum;
+    $("#total-score-num").text(totalScoreNum);
+    $("win-lose-message").text("");
+    setWinLose();
+});
+
+("#Red-Crystal").on("click", function(){
+    totalScoreNum += crystalRedNum;
+    $("#total-score-num").text(totalScoreNum);
+    $("win-lose-num").text(totalScoreNum);
+    setWinLose();
+});
 
 }
